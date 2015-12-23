d9b8c2729b5adccac4ce55e347ba4548954a47f0
ravi2-23606 <ravitejakodali@yahoo.com>
2015-12-22 20:14:16 -0500

    First commit

---
+++ cloud/main.js
@@ -0,0 +1,10 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+	console.log('Ran cloud function.');
+	response.success("Hello world!");
+});
+Parse.Cloud.define("update", function(request,response) {
+	response.success("Done")
+});
